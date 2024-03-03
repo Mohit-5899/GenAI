843959a0219537e82c6ac1a6c5e38edcd5953fc8
MOHIT MANDAWAT <69812944+Mohit-5899@users.noreply.github.com>
2024-03-03 15:11:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+import base64
+import streamlit as st
+import os
+import io
+from PIL import Image 
+import pdf2image
+import google.generativeai as genai
